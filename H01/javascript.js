27e63a65e5bfce96f995449a482ce28a8787e621
koenGF <2144264@talnet.nl>
2020-11-12 17:46:47 +0100

    finished H01

--- H01/javascript.js
+++ H01/javascript.js
@@ -1,4 +1,16 @@
-var a = 10;
+let a = 10;
 if (a > 9) {
-    console.log("hello world")
+    console.log("hello world");
 }
+
+for (let i = 0; i < 10; i++) {
+    console.log("i is nu " + i);
+}
+
+var object = {
+    shape: "round",
+    color: "pink",
+    texture: "soft"
+}
+
+console.log(object.color);
