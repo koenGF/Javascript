cd55df9673366aa2a730f457c44bed678582d5e7
koenGF <2144264@talnet.nl>
2020-11-12 17:51:11 +0100

    small changes

--- H01/javascript.js
+++ H01/javascript.js
@@ -1,10 +1,10 @@
 let a = 10;
 if (a > 9) {
-    console.log("hello world");
+    console.log("a is more than 9");
 }
 
 for (let i = 0; i < 10; i++) {
-    console.log("i is nu " + i);
+    console.log("i is " + i);
 }
 
 var object = {
