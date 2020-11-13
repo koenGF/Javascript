d06995d4ca7133d5e16b23e06e76802f7cce4b69
koenGF <2144264@talnet.nl>
2020-11-13 12:07:13 +0100

    H02 WIP

---
+++ H02/stijl.js
@@ -0,0 +1,9 @@
+document.getElementById("bl1").style.backgroundColor = "turquoise";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
+
+document.getElementById("bl2").style.position = "relative";
+document.getElementById("bl2").style.top = "20px";
+document.getElementById("bl3").style.position = "relative";
+document.getElementById("bl3").style.top = "60px";
---
+++ H02/vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel";
+document.getElementById("subkop").innerText = "Dit is de subkop";
+document.getElementById("aap1").src = "img/2.png";
+document.getElementById("aap2").src = "img/3.png";
