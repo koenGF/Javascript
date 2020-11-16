3cda03d78e8195e088f3148ef825487c6b89afc1
koenGF <2144264@talnet.nl>
2020-11-16 11:12:42 +0100

    finished H02

--- H02/stijl.js
+++ H02/stijl.js
@@ -1,9 +1,17 @@
+//background color
 document.getElementById("bl1").style.backgroundColor = "turquoise";
 document.getElementById("bl2").style.backgroundColor = "green";
 document.getElementById("bl3").style.backgroundColor = "yellow";
 document.getElementById("bl4").style.backgroundColor = "red";
 
+//position
 document.getElementById("bl2").style.position = "relative";
 document.getElementById("bl2").style.top = "20px";
 document.getElementById("bl3").style.position = "relative";
 document.getElementById("bl3").style.top = "60px";
+
+//textalign
+document.getElementById("bl1").style.paddingTop = "20px";
+document.getElementById("bl1").style.textAlign = "center";
+document.getElementById("bl2").style.textAlign = "center";
+document.getElementById("bl3").style.textAlign = "right";
