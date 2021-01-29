c8fb159c9b4c31e646c844f2bed8244282a29a4b
Koen <2144264@talnet.nl>
2021-01-29 10:15:31 +0100

    dice game finished

--- dicegame/script.js
+++ dicegame/script.js
@@ -1,122 +1,138 @@
 "use strict";
 
+/* HTML-ELEMENTEN PER GAME-ATTRIBUUT */
 const rollDiceButton = document.querySelector(".btn--roll");
 const holdButton = document.querySelector(".btn--hold");
 const newGameButton = document.querySelector(".btn--new");
 const diceImg = document.querySelector(".dice");
-let currentPlayer = 0;
 
-console.log(newGameButton);
-//ELEMENTEN
-const player0ScoreElement = document.querySelector("#score--0");
-const player1ScoreElement = document.querySelector("#score--1");
-const player0CurrentElement = document.querySelector("#current--0");
-const player1CurrentElement = document.querySelector("#current--1");
+/* HTML-ELEMENTEN PER SPELER*/
+const player0BankScoreElement = document.querySelector("#score--0");
+const player1BankScoreElement = document.querySelector("#score--1");
+const player0CurrentScoreElement = document.querySelector("#current--0");
+const player1CurrentScoreElement = document.querySelector("#current--1");
 const player0SectionElement = document.querySelector(".player--0");
 const player1SectionElement = document.querySelector(".player--1");
-//SCORES
-let currentScorePlayer0 = 0;
-let currentScorePlayer1 = 0;
-let bankScorePlayer0 = 0;
-let bankScorePlayer1 = 0;
-let activeGame = true;
 
+/* GAME-SETUP */
+let currentScorePlayer0;
+let currentScorePlayer1;
+let bankScorePlayer0;
+let bankScorePlayer1;
+let activeGame;
+let currentPlayer;
+
+/* ADD EVENTLISTENERS TO GAME-ATTRIBUTEN */
 rollDiceButton.addEventListener("click", rollDice);
-holdButton.addEventListener("click", addToBank);
+holdButton.addEventListener("click", actionHoldButton);
 newGameButton.addEventListener("click", newGame);
 
-function rollDice(){
-    if (!activeGame) return;
-    const currentRoll = Math.floor(Math.random() * 6 + 1);
-    diceImg.src = `dice-${currentRoll}.png`; //template literal
-    //optellen bij de currentScore
-    //player0CurrentElement.textContent = currentScorePlayer0;
+/* NEW GAME */
+newGame();
 
-    //misschien overzichtelijker met eerst player 0/1, daarna roll 1/2-6. kan handiger
-    //ook goed om te weten voor winsituatie en hold
-    if (currentRoll === 1) {
-        switchPlayer();
-    }
-    else {
-        if (currentPlayer === 0) {
-            currentScorePlayer0 += currentRoll;
-            player0CurrentElement.textContent = currentScorePlayer0;
-        } else {
-            currentScorePlayer1 += currentRoll;
-            player1CurrentElement.textContent = currentScorePlayer1;
-        }
-    }
-    console.log(currentRoll);
-}
-
-function switchPlayer (){
-    currentPlayer = (currentPlayer === 0) ? 1 : 0; //ternary => opzoeken
-    resetCurrentScores();
-    player0SectionElement.classList.toggle("player--active");
-    player1SectionElement.classList.toggle("player--active");
-}
-function resetCurrentScores(){
-    currentScorePlayer0 = 0; currentScorePlayer1 = 0;
-    player0CurrentElement.textContent = currentScorePlayer0;
-    player1CurrentElement.textContent = currentScorePlayer1;
-}
-
-function addToBank() {
-    if (currentPlayer === 0) {
-        bankScorePlayer0 += currentScorePlayer0;
-        player0ScoreElement.textContent = bankScorePlayer0;
-        switchPlayer();
-    } else {
-        bankScorePlayer1 += currentScorePlayer1;
-        player1ScoreElement.textContent = bankScorePlayer1;
-        switchPlayer();
-    }
-    winCheck();
-}
-
-function winCheck() {
-    if (currentPlayer !== 0) {
-        if (bankScorePlayer0 >= 10) {
-            player0SectionElement.classList.add("player--winner");
-            activeGame = false;
-        }
-    } else {
-        if (bankScorePlayer1 >= 10) {
-            player1SectionElement.classList.add("player--winner");
-            activeGame = false;
-        }
-    }
-}
-
+/* FUNCTIONS */
 function newGame() {
-    currentScorePlayer0 = 0;
-    currentScorePlayer1 = 0;
-    bankScorePlayer0 = 0;
-    bankScorePlayer1 = 0;
-    player0CurrentElement.textContent = currentScorePlayer0;
-    player1CurrentElement.textContent = currentScorePlayer1;
-    player0ScoreElement.textContent = bankScorePlayer0;
-    player1ScoreElement.textContent = bankScorePlayer1;
-    currentPlayer = 0;
+    resetClasses();
+    gameSetup();
+    resetHTMLElements();
+}
+function resetClasses(){
     player0SectionElement.classList.add("player--active");
     player1SectionElement.classList.remove("player--active");
     player0SectionElement.classList.remove("player--winner");
     player1SectionElement.classList.remove("player--winner");
+}
+function gameSetup(){
+    currentScorePlayer0 = 0;
+    currentScorePlayer1 = 0;
+    bankScorePlayer0 = 0;
+    bankScorePlayer1 = 0;
+    currentPlayer = 0;
     activeGame = true;
 }
-/* reset bank & current
+function resetHTMLElements(){
+    player0CurrentScoreElement.textContent = currentScorePlayer0;
+    player1CurrentScoreElement.textContent = currentScorePlayer1;
+    player0BankScoreElement.textContent = bankScorePlayer0;
+    player1BankScoreElement.textContent = bankScorePlayer1;
+}
 
-ROLL DICE
-roll dice (1-6)
-1: reset current, change player
-2-6: add to current-->
+/* GAME LOGIC */
+function rollDice(){
+    if (!activeGame) return;
+    const currentRoll = rollDiceAndChangeImage();
+    processScore(checkScore(currentRoll), currentRoll);
+}
 
-HOLD
-add current to bank, clear current
->=60? end game
+function rollDiceAndChangeImage(){
+    let currentRoll = Math.floor(Math.random() * 6 + 1);
+    diceImg.src = `dice-${currentRoll}.png`;
+    return currentRoll;
+}
 
+function checkScore(score){
+    return (score !== 1);
+}
+function processScore(correct, currentRoll){
+    (correct) ? addToScoreCorrectPlayer(currentRoll): switchPlayer();
+}
+function addToScoreCorrectPlayer(currentRoll){
+    (currentPlayer === 0) ? addScoreToPlayerZero(currentRoll) : addScoreToPlayerOne(currentRoll);
+}
+function addScoreToPlayerZero (currentRoll){
+    currentScorePlayer0 += currentRoll;
+    player0CurrentScoreElement.textContent = currentScorePlayer0;
+}
+function addScoreToPlayerOne(currentRoll){
+    currentScorePlayer1 += currentRoll;
+    player1CurrentScoreElement.textContent = currentScorePlayer1;
+}
 
-wie aan de beurt
-score aan speler toevoegen
-wissel speler bij 1
-*/
+function switchPlayer (){
+    currentPlayer = (currentPlayer === 0) ? 1 : 0;
+    resetCurrentScores();
+    toggleActivePlayerBackground();
+}
+function resetCurrentScores(){
+    currentScorePlayer0 = 0; currentScorePlayer1 = 0;
+    player0CurrentScoreElement.textContent = currentScorePlayer0;
+    player1CurrentScoreElement.textContent = currentScorePlayer1;
+}
+function toggleActivePlayerBackground() {
+    player0SectionElement.classList.toggle("player--active");
+    player1SectionElement.classList.toggle("player--active");
+}
+
+function actionHoldButton() {
+    addToBank();
+    winCheck();
+    switchPlayer();
+}
+function addToBank() {
+    if (!activeGame) return;
+    (currentPlayer === 0) ? addToPlayer0Bank() : addToPlayer1Bank();
+}
+function addToPlayer0Bank() {
+    bankScorePlayer0 += currentScorePlayer0;
+    player0BankScoreElement.textContent = bankScorePlayer0;
+}
+function addToPlayer1Bank() {
+    bankScorePlayer1 += currentScorePlayer1;
+    player1BankScoreElement.textContent = bankScorePlayer1;
+}
+
+function winCheck() {
+    (currentPlayer === 0) ? winCheckPlayer0() :  winCheckPlayer1();
+}
+function winCheckPlayer0() {
+    if (bankScorePlayer0 >= 10) {
+        player0SectionElement.classList.add("player--winner");
+        activeGame = false;
+    }
+}
+function winCheckPlayer1() {
+    if (bankScorePlayer1 >= 10) {
+        player1SectionElement.classList.add("player--winner");
+        activeGame = false;
+    }
+}
